ea863b182c78745b62c26c885c9739f60a605515
Neil Rogers <rogerne@btinternet.com>
2014-04-09 20:35:28 +0100

    Then tests

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -1,2 +1,23 @@
-Given /^I am not yet playing$/ do
+class Output
+  def messages
+    @messages ||= []
+  end
+
+  def puts(message)
+    messages << message
+  end
+end
+
+def output
+  @output ||= Output.new
+end
+
+# features/step_definitions/codebreaker_steps.rb
+When /^I start a new game$/ do
+  game = Codebreaker::Game.new(output) # using output method
+  game.start
+end
+
+Then /^I should see "([^\"]*)"$/ do |message|
+  output.messages.should include(message)
 end
